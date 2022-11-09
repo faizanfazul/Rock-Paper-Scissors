d3cd4ab300d50329a35049b239a3a57ba59a1a85
faizanfazul <faizanfazul6@gmail.com>
2022-11-09 23:50:04 +0500

    README.md

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Rock-Paper-Scissors 🪨|📃|✂️.
 ### Another project given by The Odin Project. https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 <br>
+<<<<<<< HEAD
 Play This Online https://faizanfazul.github.io/Rock-Paper-Scissors/ <br>
 
+=======
+
+### Play this Online at: https://faizanfazul.github.io/Rock-Paper-Scissors/
+<br>
+>>>>>>> 5106bd5 (README.md)
 1.Here a person is against the computer lets see who wins!
 
 <br>
--- app.js
+++ app.js
@@ -5,34 +5,39 @@ function game() {
     return random;
   }
 
-  
+  const Error =
+    "Typing mistake! No worries next time be careful.\n1- Check that you typed the spelling correctly.\n2- only small letters are allow not even capitalized.";
+
   function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
       return "It's a draw! you both selected same.";
     } else if (playerSelection === "rock" && computerSelection === "scissor") {
       return "You win! Rock beats scissor";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-      return "You lose! Paper beats rock";
+      return "You lose! Paper beats rock\ntry again!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
       return "You win! Paper beats rock";
     } else if (playerSelection === "paper" && computerSelection === "scissor") {
-      return "You lose! Scissor beat paper";
+      return "You lose! Scissor beat paper\ntry again!";
     } else if (playerSelection === "scissor" && computerSelection === "paper") {
       return "You win! Scissor beat paper";
     } else if (playerSelection === "scissor" && computerSelection === "rock") {
-      return "You lose! Rock beats scissor";
+      return "You lose! Rock beats scissor\ntry again!";
+    } else {
+      return Error;
     }
   }
 
   const computerSelection = computerPlay();
-  const playerSelection = prompt("Choose your weapon | rock🪨, paper📃 or scissor✂️");
+  const playerSelection = prompt(
+    "Choose your weapon | rock🪨, paper📃 or scissor✂️"
+  );
   console.warn("You Selected " + '"' + playerSelection + '"');
   console.error("Computer Selected " + '"' + computerSelection + '"');
-  console.log("Results: "+playRound(playerSelection, computerSelection));
-
+  console.log("Results: " + playRound(playerSelection, computerSelection));
 }
 
 for (let i = 0; i < 5; i++) {
-   game()
+  game();
 }
-console.warn("This is END Please Refresh the page to play again")
+console.warn("This is END Please Refresh the page to play again.");
